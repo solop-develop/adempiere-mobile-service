cdc71ba4c34977309d575963dace96126fe4d9d3
yamelsenih <ysenih@erpya.com>
2024-03-06 11:53:15 -0400

    Add examples for menu

--- src/main/java/org/spin/mobile_service/service/appoinment/AppoinmentService.java
+++ src/main/java/org/spin/mobile_service/service/appoinment/AppoinmentService.java
@@ -14,6 +14,14 @@
  ************************************************************************************/
 package org.spin.mobile_service.service.appoinment;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Optional;
+
+import org.adempiere.core.domains.models.I_C_NonBusinessDay;
+import org.compiere.model.MNonBusinessDay;
+import org.compiere.model.Query;
+import org.compiere.util.Env;
 import org.spin.proto.mobile.appoinment.GetListRequest;
 import org.spin.proto.mobile.appoinment.GetListResponse;
 import org.spin.proto.mobile.appoinment.ListData;
@@ -22,9 +30,25 @@ import org.spin.proto.mobile.appoinment.ListItem;
 public class AppoinmentService {
 	
 	public static GetListResponse getList(GetListRequest chekToken) {
-		ListData.Builder data = ListData.newBuilder()
-				.addItems(ListItem.newBuilder().setId(1).setTitle("New Year").setDate("January 1").setDay("Sunday").setStartDate("January 1, 2023").setAttachmentFileId("https://hrm.onesttech.com/static/blank_small.png"))
-				;
+		ListData.Builder data = ListData.newBuilder();
+		new Query(Env.getCtx(), I_C_NonBusinessDay.Table_Name, "Date1 >= ?", null)
+		.setParameters(new Timestamp(System.currentTimeMillis()))
+		.setOrderBy(I_C_NonBusinessDay.COLUMNNAME_Date1)
+		.getIDsAsList()
+		.forEach(nonBusinessDayId -> {
+			MNonBusinessDay nonBusinessDay = new MNonBusinessDay(Env.getCtx(), nonBusinessDayId, null);
+			data.addItems(
+					ListItem.newBuilder()
+					.setId(nonBusinessDayId)
+					.setTitle(Optional.ofNullable(nonBusinessDay.getName()).orElse("--"))
+					.setDate(new SimpleDateFormat("MMMM dd").format(nonBusinessDay.getDate1()))
+					.setDay(new SimpleDateFormat("EEEE").format(nonBusinessDay.getDate1()))
+					.setStartDate(new SimpleDateFormat("MMMM dd, yyyy").format(nonBusinessDay.getDate1()))
+					.setAttachmentFileId("https://hrm.onesttech.com/static/blank_small.png"))
+			;
+		});
+		
+				
 		
 		return GetListResponse.newBuilder()
 				.setData(data)
